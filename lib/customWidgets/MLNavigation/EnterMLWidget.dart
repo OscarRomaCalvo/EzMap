23978fab6aea436022ef72743470c91cae7f7b85
OscarRomaFever <oscar.roma@feverup.com>
2024-05-19 13:30:51 +0200

    Update Enter Metro and Metro Ligero

--- lib/customWidgets/MLNavigation/EnterMLWidget.dart
+++ lib/customWidgets/MLNavigation/EnterMLWidget.dart
@@ -1,3 +1,4 @@
+import 'package:ez_maps/customWidgets/ImageButton.dart';
 import 'package:flutter/material.dart';
 import '../../services/TextReader.dart';
 import '../CustomButton.dart';
@@ -20,36 +21,45 @@ class _EnterMLWidgetState extends State<EnterMLWidget> {
   }
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text(
-          "Entra a la estación de metro ligero",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-          textAlign: TextAlign.center,
-          softWrap: true,
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFFF5F5F5),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Entra a la estación de metro ligero",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+            const SizedBox(height: 20),
+            const Image(
+              image: AssetImage("assets/images/metroLigero/ml-logo.png"),
+              width: double.infinity,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              widget.originStation,
+              style: const TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+            const SizedBox(height: 40),
+            ImageButton(imagePath: "assets/images/ARASAACPictograms/nextButton.png", onPressed: widget.startMLNavigation, size: 100,),
+          ],
         ),
-        const SizedBox(height: 20),
-        const Image(
-          image: AssetImage("assets/images/metroLigero/ml-logo.png"),
-          width: double.infinity,
-        ),
-        const SizedBox(height: 20),
-        Text(
-          widget.originStation,
-          style: const TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
-          textAlign: TextAlign.center,
-          softWrap: true,
-        ),
-        const SizedBox(height: 20),
-        CustomButton("SIGUIENTE", widget.startMLNavigation, true),
-      ],
+      ),
     );
   }
 }
--- lib/customWidgets/MetroNavigation/EnterMetroWidget.dart
+++ lib/customWidgets/MetroNavigation/EnterMetroWidget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../services/TextReader.dart';
 import '../CustomButton.dart';
+import '../ImageButton.dart';
 
 class EnterMetroWidget extends StatefulWidget {
   final String originStation;
@@ -22,36 +23,45 @@ class _EnterMetroWidgetState extends State<EnterMetroWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text(
-          "Entra a la estación de metro",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-          textAlign: TextAlign.center,
-          softWrap: true,
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFFF5F5F5),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Entra a la estación de metro",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+            const SizedBox(height: 20),
+            const Image(
+              image: AssetImage("assets/images/metro/metro-logo.png"),
+              width: double.infinity,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              widget.originStation,
+              style: const TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+            const SizedBox(height: 40),
+            ImageButton(imagePath: "assets/images/ARASAACPictograms/nextButton.png", onPressed: widget.startMetroNavigation, size: 100,),
+          ],
         ),
-        const SizedBox(height: 20),
-        const Image(
-          image: AssetImage("assets/images/metro/metro-logo.png"),
-          width: double.infinity,
-        ),
-        const SizedBox(height: 20),
-        Text(
-          widget.originStation,
-          style: const TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
-          textAlign: TextAlign.center,
-          softWrap: true,
-        ),
-        const SizedBox(height: 20),
-        CustomButton("SIGUIENTE", widget.startMetroNavigation, true),
-      ],
+      ),
     );
   }
 }
